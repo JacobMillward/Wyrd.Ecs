de2e59f044de0f906567dc3dd312d89af30cadb6
Jacob Millward-Yuan <jacob@millward.dev>
2026-07-16 20:28:31 +0100

    test: cover a three-component QuerySystem, grounded against TransceiverDuplexSystem's shape

--- src/Wyrd.Ecs.Tests/QuerySystemTests.cs
+++ src/Wyrd.Ecs.Tests/QuerySystemTests.cs
@@ -16,6 +16,30 @@ internal sealed partial class EnergyDrainSystem : QuerySystem<Energy>
     }
 }
 
+internal struct Transceiver : IComponent
+{
+    public float Bandwidth;
+}
+
+internal struct Outbox : IComponent
+{
+    public float SendProgress;
+}
+
+internal struct Inbox : IComponent
+{
+    public float ReceiveProgress;
+}
+
+internal sealed partial class ThreeComponentSystem : QuerySystem<Transceiver, Outbox, Inbox>
+{
+    protected override void Execute(World world, ulong tick, ref Transceiver transceiver, ref Outbox outbox, ref Inbox inbox)
+    {
+        outbox.SendProgress += transceiver.Bandwidth;
+        inbox.ReceiveProgress += transceiver.Bandwidth;
+    }
+}
+
 public class QuerySystemTests
 {
     [Fact]
@@ -46,5 +70,24 @@ public class QuerySystemTests
 
         world.GetComponent<Energy>(plain).Current.Should().Be(45f);
         world.GetComponent<Energy>(tagged).Current.Should().Be(18f);
+    }
+
+    [Fact]
+    public void QuerySystem_ThreeComponentArity_RunsAndWritesThroughAllThree()
+    {
+        var world = new World();
+        var entity = world.CreateEntity();
+        world.AddComponent<Transceiver>(entity) = new Transceiver { Bandwidth = 3f };
+        world.AddComponent<Outbox>(entity) = new Outbox { SendProgress = 0f };
+        world.AddComponent<Inbox>(entity) = new Inbox { ReceiveProgress = 0f };
+        var missingOne = world.CreateEntity();
+        world.AddComponent<Transceiver>(missingOne);
+        world.AddComponent<Outbox>(missingOne);
+
+        var system = new ThreeComponentSystem();
+        system.RunOnce(world, tick: 1);
+
+        world.GetComponent<Outbox>(entity).SendProgress.Should().Be(3f);
+        world.GetComponent<Inbox>(entity).ReceiveProgress.Should().Be(3f);
     }
 }
