026311155e3fb57a5c8d1862ee45d1a7a6505794
Jacob Millward-Yuan <jacob@millward.dev>
2026-07-16 08:49:02 +0100

    test: add structural-change benchmarks

---
+++ src/Wyrd.Ecs.Benchmarks/Friflo/StructuralChangeBenchmarks.cs
@@ -0,0 +1,39 @@
+using BenchmarkDotNet.Attributes;
+using Friflo.Engine.ECS;
+
+namespace Wyrd.Ecs.Benchmarks.Friflo;
+
+[MemoryDiagnoser]
+public class StructuralChangeBenchmarks
+{
+    private EntityStore _store = null!;
+    private Entity _entity;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _store = new EntityStore();
+        _entity = _store.CreateEntity(new Position(), new Velocity());
+    }
+
+    [Benchmark(Baseline = true)]
+    public void MutateExistingComponent()
+    {
+        ref var position = ref _entity.GetComponent<Position>();
+        position.X += 0f;
+    }
+
+    [Benchmark]
+    public void AddRemoveComponent_ArchetypeMove()
+    {
+        _entity.AddComponent(new Payload8());
+        _entity.RemoveComponent<Payload8>();
+    }
+
+    [Benchmark]
+    public void AddRemoveTag_ArchetypeMove()
+    {
+        _entity.AddTag<Marker>();
+        _entity.RemoveTag<Marker>();
+    }
+}
